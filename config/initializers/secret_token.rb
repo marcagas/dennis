97afb7d24135f566de30f50447f2f2233f429c73
marc lambert agas <marclambertagas@gmail.com>
2014-02-13 21:38:43 +0800

    Initial files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dennis::Application.config.secret_token = 'adc86d543ea301610fa88c870080422a4501336a3d3741429db9591787fe2d79131c8d80f3a61ffb46667714459344fb6c69c8bfa56ff72e089338a221d5a23b'
